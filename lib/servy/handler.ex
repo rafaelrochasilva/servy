879811e73321e44417b5dc75ba9e6a605fd0adea
Rafael Rocha <rafaelrochasilva@gmail.com>
2017-11-09 09:52:47 -0200

    Add status codes to the request

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -13,24 +13,39 @@ defmodule Servy.Handler do
       |> List.first
       |> String.split
 
-    %{ method: method, path: path, resp_body: ""}
+    %{ method: method, status: nil, path: path, resp_body: ""}
   end
 
   def route(%{method: "GET", path: "/wildzoo"} = conv) do
-    %{ conv | resp_body: "Bears, Lions, Tigers" }
+    %{ conv | status: 200, resp_body: "Bears, Lions, Tigers" }
   end
 
   def route(%{method: "GET", path: "/bears"} = conv) do
-    %{ conv | resp_body: "Yogi, Panda, Paddington" }
+    %{ conv | status: 200, resp_body: "Yogi, Panda, Paddington" }
   end
 
-  def format_response(%{ method: _, path: _, resp_body: resp_body }) do
+  def route(%{method: "GET", path: path} = conv) do
+    %{ conv | status: 404, resp_body: "No #{path} here!"}
+  end
+
+  def format_response(%{ status: code, resp_body: resp_body }) do
     """
-    HTTP/1.1 200 OK
+    HTTP/1.1 #{code} #{status_reason(code)}
     Content-Type: text/html
     Content-Length: #{String.length(resp_body)}
 
     #{resp_body}
     """
+  end
+
+  defp status_reason(code) do
+    %{
+      200 => "OK",
+      201 => "Created",
+      401 => "Unauthorized",
+      403 => "Forbidden",
+      404 => "Not Found",
+      500 => "Internal Server Error"
+    }[code]
   end
 end
--- test/servy/handler_test.exs
+++ test/servy/handler_test.exs
@@ -21,6 +21,26 @@ defmodule HandlerTest do
     assert Servy.Handler.handle(request) == expected_response
   end
 
+  test "handler a unknow path request" do
+    request = """
+    GET /tigers HTTP/1.1
+    Host: example.com
+    User-Agent: ExampleBrowser/1.0
+    Accept: */*
+
+    """
+
+    expected_response = """
+    HTTP/1.1 404 Not Found
+    Content-Type: text/html
+    Content-Length: 16
+
+    No /tigers here!
+    """
+
+    assert Servy.Handler.handle(request) == expected_response
+  end
+
   test "parse request" do
     request = """
     GET /wildzoo HTTP/1.1
@@ -30,25 +50,32 @@ defmodule HandlerTest do
 
     """
 
-    assert Servy.Handler.parse(request) == %{ method: "GET", path: "/wildzoo", resp_body: "" }
+    assert Servy.Handler.parse(request) == %{ method: "GET", status: nil, path: "/wildzoo", resp_body: "" }
   end
 
   test "route the parsed request" do
-    parsed_request = %{method: "GET", path: "/wildzoo", resp_body: ""}
-    expected_route =  %{method: "GET", path: "/wildzoo", resp_body: "Bears, Lions, Tigers"}
+    parsed_request = %{method: "GET", status: nil, path: "/wildzoo", resp_body: ""}
+    expected_route =  %{method: "GET", status: 200, path: "/wildzoo", resp_body: "Bears, Lions, Tigers"}
 
     assert Servy.Handler.route(parsed_request) == expected_route
   end
 
   test "routes to a bear path" do
-    parsed_request = %{method: "GET", path: "/bears", resp_body: ""}
-    expected_route =  %{method: "GET", path: "/bears", resp_body: "Yogi, Panda, Paddington"}
+    parsed_request = %{method: "GET", status: nil, path: "/bears", resp_body: ""}
+    expected_route =  %{method: "GET", status: 200, path: "/bears", resp_body: "Yogi, Panda, Paddington"}
+
+    assert Servy.Handler.route(parsed_request) == expected_route
+  end
+
+  test "routes an unknow route" do
+    parsed_request = %{method: "GET", status: nil, path: "/tigers", resp_body: ""}
+    expected_route =  %{method: "GET", status: 404, path: "/tigers", resp_body: "No /tigers here!"}
 
     assert Servy.Handler.route(parsed_request) == expected_route
   end
 
   test "formats the response" do
-    route_response =  %{method: "GET", path: "/wildzoo", resp_body: "Bears, Lions, Tigers"}
+    route_response =  %{method: "GET", status: 200, path: "/wildzoo", resp_body: "Bears, Lions, Tigers"}
     expected_response = """
     HTTP/1.1 200 OK
     Content-Type: text/html
