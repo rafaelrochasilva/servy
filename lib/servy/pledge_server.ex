624dbba0e6d2b6c893d283c32f6ebf7ce68ca9f8
Rafael Rocha <rafaelrochasilva@gmail.com>
2017-12-12 09:33:44 -0200

    Add the total of pledges received

--- lib/servy/pledge_server.ex
+++ lib/servy/pledge_server.ex
@@ -7,7 +7,7 @@ defmodule Servy.PledgeServer do
   @process_name :pledge_server
 
   def start do
-    pid = spawn(__MODULE__, :listen_loop, [[]])
+    pid = spawn(__MODULE__, :listen_loop, [[], 0])
     Process.register(pid, :pledge_server)
     pid
   end
@@ -26,18 +26,36 @@ defmodule Servy.PledgeServer do
     receive do {:response, pledges} -> pledges end
   end
 
-  def listen_loop(state) do
+  @spec total_pledged() :: number
+  def total_pledged do
+    send @process_name, {self(), :total_pledged}
+
+    receive do {:total_pledged, total} -> total end
+  end
+
+  def listen_loop(state, total) do
     receive do
       {sender, :create_pledge, name, amount} ->
         {:ok, id} = send_pledge_to_service(name, amount)
         new_state =
           [{name, amount} | state]
           |> Enum.take(3)
+        IO.puts "#{total} + #{amount}"
+        new_total = total + amount
         send sender, {:response, id}
-        listen_loop(new_state)
+        listen_loop(new_state, new_total)
+
       {sender, :recent_pledges} ->
         send sender, {:response, state}
-        listen_loop(state)
+        listen_loop(state, total)
+
+      {sender, :total_pledged} ->
+        send sender, {:total_pledged, total}
+        listen_loop(state, total)
+
+      unexpected ->
+        IO.puts "Unexpected message: #{inspect unexpected}"
+        listen_loop(state, total)
     end
   end
 
--- test/servy/pledge_server_test.exs
+++ test/servy/pledge_server_test.exs
@@ -4,7 +4,7 @@ defmodule Servy.PledgeServerTest do
   test "creates a pledge for a given name and amount" do
     pid = Servy.PledgeServer.start()
 
-    assert Regex.match?(~r/pledge/, Servy.PledgeServer.create_pledge("larry", "10"))
+    assert Regex.match?(~r/pledge/, Servy.PledgeServer.create_pledge("larry", 10))
     Process.exit(pid, :kill)
   end
 
@@ -25,6 +25,17 @@ defmodule Servy.PledgeServerTest do
     expected_response = [{"joe", 30}, {"anna", 50}, {"bruce", 20}]
 
     assert Servy.PledgeServer.recent_pledges() == expected_response
+    Process.exit(pid, :kill)
+  end
+
+  test "returns the total of pledges" do
+    pid = Servy.PledgeServer.start()
+    Servy.PledgeServer.create_pledge("larry", 10)
+    Servy.PledgeServer.create_pledge("bruce", 20)
+    Servy.PledgeServer.create_pledge("anna", 50)
+    Servy.PledgeServer.create_pledge("joe", 30)
+
+    assert Servy.PledgeServer.total_pledged() == 110
     Process.exit(pid, :kill)
   end
 end
